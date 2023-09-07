efe9d787a774891aa87c5f6361e5500390e40fc4
Guy S <guysinger@outlook.com>
2023-09-07 14:24:40 +0300

    changes to captions.py, definitions, and galleries
    these changes only affect the way captioning is done for galleries display.

--- fastdup/definitions.py
+++ fastdup/definitions.py
@@ -90,9 +90,7 @@ EFFICIENTNET_MODEL_DIM = 1000
 RESNET50_MODEL = "https://github.com/onnx/models/raw/main/vision/classification/resnet/model/resnet50-v1-12.onnx"
 RESNET50_MODEL_DIM = 1000
 
-CAPTION_MODEL1_NAME = 'automatic'
-CAPTION_MODEL2_NAME = 'blip'
-CAPTION_MODEL3_NAME = 'blip2'
+CAPTION_MODEL_NAMES = ['automatic', 'vitgpt2', 'blip', 'blip2']
 VQA_MODEL1_NAME = "indoors_outdoors"
 AGE_LABEL1_NAME = 'age'
 
--- fastdup/galleries.py
+++ fastdup/galleries.py
@@ -85,15 +85,9 @@ def find_label(get_label_func, df, in_col, out_col, kwargs=None):
                 df[out_col] = df_labels['label']
             elif get_label_func in df.columns:
                 df[out_col] = df['label']
-            elif get_label_func == CAPTION_MODEL1_NAME:
+            elif get_label_func in CAPTION_MODEL_NAMES:
                 from fastdup.captions import generate_labels
-                df[out_col] = generate_labels(df[in_col], kwargs)
-            elif get_label_func == CAPTION_MODEL2_NAME:
-                from fastdup.captions import generate_blip_labels
-                df[out_col] = generate_blip_labels(df[in_col], kwargs)
-            elif get_label_func == CAPTION_MODEL3_NAME:
-                from fastdup.captions import generate_blip2_labels
-                df[out_col] = generate_blip2_labels(df[in_col], kwargs)
+                df[out_col] = generate_labels(df[in_col], get_label_func)
             elif get_label_func == VQA_MODEL1_NAME:
                 from fastdup.captions import generate_vqa_labels
                 df[out_col] = generate_vqa_labels(df[in_col], "Is the photo taken indoors or outdoors", kwargs)
@@ -110,6 +104,10 @@ def find_label(get_label_func, df, in_col, out_col, kwargs=None):
             df[out_col] = df[in_col].apply(lambda x: get_label_func(x))
         else:
             assert False, f"Failed to understand get_label_func type {type(get_label_func)}"
+
+    if kwargs is not None and 'debug_labels' in kwargs:
+        print(df.head())
+    return df
 
     if kwargs is not None and 'debug_labels' in kwargs:
         print(df.head())
