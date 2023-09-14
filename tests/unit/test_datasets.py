ad950661a8eafce4a595e7489a772707d018fda2
dnth <dickson.neoh@gmail.com>
2023-09-14 11:17:18 +0800

    update test

--- tests/unit/test_datasets.py
+++ tests/unit/test_datasets.py
@@ -10,45 +10,36 @@ def dataset():
     return FastdupHFDataset("zh-plus/tiny-imagenet", split="train[:5%]")
 
 
-def test_load_dataset(dataset):
-    assert dataset.info.dataset_name == "tiny-imagenet"
-    assert dataset.cache_dir == HF_DATASETS_CACHE
+def test_instantiating_dataset_with_valid_dataset_name_and_split(dataset):
+    assert isinstance(dataset, FastdupHFDataset)
     assert dataset.img_key == "image"
     assert dataset.label_key == "label"
     assert dataset.jpg_save_dir == "jpg_images"
+    assert dataset.cache_dir == HF_DATASETS_CACHE
+    assert dataset.info.dataset_name == "tiny-imagenet"
+    assert len(dataset) == 5000
 
 
-def test_img_dir(dataset):
+def test_img_dir_exists(dataset):
+    assert os.path.isdir(dataset.img_dir)
     assert dataset.img_dir == os.path.join(
         HF_DATASETS_CACHE, "tiny-imagenet", "jpg_images"
     )
 
 
-def test_dataset_len(dataset):
-    assert len(dataset) == 5000
+# Accessing img_dir property after successful instantiation
+def test_accessing_img_dir_property(dataset):
+    assert isinstance(dataset.img_dir, str)
+    assert os.path.exists(dataset.img_dir)
 
 
-def test_img_dir_structure(dataset):
-    expected_img_dir = os.path.join(
-        dataset.cache_dir, dataset.hf_dataset.info.dataset_name, dataset.jpg_save_dir
-    )
-    assert dataset.img_dir == expected_img_dir
+# Accessing annotations property after successful instantiation
+def test_accessing_annotations_property_after_successful_instantiation(dataset):
+    annotations = dataset.annotations
+    assert isinstance(annotations, pd.DataFrame)
+    assert "filename" in annotations.columns
+    assert "label" in annotations.columns
 
-
-def test_img_dir_exists(dataset):
-    assert os.path.isdir(dataset.img_dir)
-
-
-def test_annotations_columns(dataset):
-    assert "filename" in dataset.annotations.columns
-    assert "label" in dataset.annotations.columns
-
-
-def test_annotations_type(dataset):
-    assert isinstance(dataset.annotations, pd.DataFrame)
-
-
-def test_annotations_content(dataset):
     sample_row = dataset.annotations.iloc[0]
     assert os.path.exists(sample_row["filename"])
     assert isinstance(sample_row["label"], str)
